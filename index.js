a18bd17dce1430c0e80e46594b2e5ef8f5a5d43b
Thibaut Van Spaandonck <thibaut@urge2code.com>
2017-10-10 14:11:12 +0200

    Passing arguments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const schema = buildSchema(`
   type Query {
     quoteOfTheDay: String
     random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
@@ -16,7 +16,13 @@ const root = {
   quoteOfTheDay: () =>
     Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
   random: () => Math.random(),
-  rollThreeDice: () => [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
+  rollDice: ({ numDice, numSides }) => {
+    const output = []
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  }
 }
 
 const app = express()
